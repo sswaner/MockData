f963710689d32605e0489f6639ec583b761bbb1b
Josh Kunz <josh@kunz.xyz>
2015-12-26 13:40:04 -0600

    Make config.py use path of config file...
    Instead of a hardcoded path name.

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-root_path = '/Users/shawnswaner/MockData'
+import os
+root_path = os.path.dirname(os.path.abspath(__file__))
 
-pickle_path = root_path + '/pickles/'
-
-source_path = root_path + '/source/'
-
+pickle_path = os.path.join(root_path, 'pickles/')
+source_path = os.path.join(root_path, 'source/')
 
 demo_data = {'users' : 'users.pkl' }
-
-
